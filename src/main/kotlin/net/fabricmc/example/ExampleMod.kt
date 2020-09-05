19514e43f5db550e1dae77ff1debfd242aeda966
dontcampy <dontcampy@gmail.com>
2020-09-05 12:41:22 +0800

    Upgrade to kotlin 1.4.0

--- src/main/kotlin/net/fabricmc/example/ExampleMod.kt
+++ src/main/kotlin/net/fabricmc/example/ExampleMod.kt
@@ -1,13 +1,21 @@
 package net.fabricmc.example
 
+import org.apache.logging.log4j.LogManager
+
 // For support join https://discord.gg/v6v4pMv
 
 @Suppress("unused")
 fun init() {
-    // This code runs as soon as Minecraft is in a mod-load-ready state.
-    // However, some things (like resources) may still be uninitialized.
-    // Proceed with mild caution.
+  // This code runs as soon as Minecraft is in a mod-load-ready state.
+  // However, some things (like resources) may still be uninitialized.
+  // Proceed with mild caution.
 
-    println("Hello Fabric world!")
+  HelloWorld().hi()
 }
 
+class HelloWorld {
+  private val logger = LogManager.getLogger(javaClass)
+  fun hi() {
+    logger.info("Hello Fabric woooooooooooooooooooooooorld!")
+  }
+}
